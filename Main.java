f4e85f466e3f4d1477395314219a8820a816bd20
Mateusz0231 <100692029+Mateusz0231@users.noreply.github.com>
2022-03-26 19:01:56 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Main 
+{
+  public static void main(String[] args) 
+  {
+        Scanner ch = new Scanner(System.in);
+        System.out.println("Podaj wysokosc choinki");
+        int wys = ch.nextInt();
+        for (int i = 1; i<=wys; i++)
+        {
+            for(int j=0;j<wys-i;j++)System.out.print(" ");
+            for (int j = 0; j<(i*2)-1;j++)System.out.print("*");
+            System.out.println(" ");
+        }
+  
+    }
+}
